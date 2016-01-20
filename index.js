72d9ac53f1b9e9f61f0378fe8d415d1d6ad4d8bf
Nathan Downs <n8downs@gmail.com>
2016-01-20 11:08:45 -0800

    Style

--- index.js
+++ index.js
@@ -10,10 +10,12 @@ function CompressionPlugin(options) {
 	options = options || {};
 	this.asset = options.asset || "[file].gz";
 	this.algorithm = options.algorithm || "gzip";
-	if(typeof this.algorithm === "string") {
+	if (typeof this.algorithm === "string") {
 		var zlib = require("zlib");
 		this.algorithm = zlib[this.algorithm];
-		if(!this.algorithm) throw new Error("Algorithm not found in zlib");
+		if (!this.algorithm) {
+			throw new Error("Algorithm not found in zlib");
+		}
 		this.algorithm = this.algorithm.bind(zlib);
 	}
 	this.regExp = options.regExp;
@@ -26,16 +28,25 @@ CompressionPlugin.prototype.apply = function(compiler) {
 	compiler.plugin("this-compilation", function(compilation) {
 		compilation.plugin("optimize-assets", function(assets, callback) {
 			async.forEach(Object.keys(assets), function(file, callback) {
-				if(this.regExp && !this.regExp.test(file)) return callback();
+				if (this.regExp && !this.regExp.test(file)) {
+					return callback();
+				}
 				var asset = assets[file];
 				var content = asset.source();
-				if(!Buffer.isBuffer(content))
+				if (!Buffer.isBuffer(content)) {
 					content = new Buffer(content, "utf-8");
+				}
 				var originalSize = content.length;
-				if(originalSize < this.threshold) return callback();
+				if (originalSize < this.threshold) {
+					return callback();
+				}
 				this.algorithm(content, function(err, result) {
-					if(err) return callback(err);
-					if(result.length / originalSize > this.minRatio) return callback();
+					if (err) {
+						return callback(err);
+					}
+					if (result.length / originalSize > this.minRatio) {
+						return callback();
+					}
 					var newFile = this.asset.replace(/\[file\]/g, file);
 					assets[newFile] = new RawSource(result);
 					callback();
